d2870e568cd840d743af2c372b0995d4551db9ff
Comandeer <vepomoc@gmail.com>
2016-09-04 00:12:37 +0200

    Updated articles list.

--- build/artslist.js
+++ build/artslist.js
@@ -3,5 +3,6 @@ module.exports = {
 	'Web (of) Intents – czego brakuje dzisiejszej Sieci?': 'http://webroad.pl/inne/3035-web-of-intents-czego-brakuje-dzisiejszej-sieci',
 	'Web Components': 'http://webroad.pl/javascript/3505-web-components',
 	'Progressive Enhancement – zapomniany fundament': 'http://webroad.pl/inne/3722-progressive-enhancement-zapomniany-fundament',
-	'To validate or not to validate – that is the question!': 'http://webroad.pl/inne/3925-validate-or-not-to-validate-that-is-the-question'
+	'To validate or not to validate – that is the question!': 'http://webroad.pl/inne/3925-validate-or-not-to-validate-that-is-the-question',
+	'Filtrowanie HTML przy pomocy selektorów CSS': 'http://devcorner.pl/filtrowanie-html-przy-pomocy-selektorow-css/'
 };
